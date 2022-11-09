a3a65c157a96523694626b5bf7e34dba0cc38920
YesNoah <98677277+YesNoah@users.noreply.github.com>
2022-11-09 01:17:16 -0500

    Add files via upload

---
+++ Term_Repo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predict if customers will subscribe to a term deposit using categorical and numerical survey data.',
+    author='Noah_Cox',
+    license='MIT',
+)
---
+++ Term_Repo/src/source.py
@@ -0,0 +1,21 @@
+import os
+os.getcwd()
+import importlib
+import data
+importlib.reload(data)
+from data import make_dataset
+importlib.reload(make_dataset)
+from data import data_utils
+X_train, X_test, y_train, y_test = make_dataset.main()
+X_train
+
+
+import models
+import models.train_model as train
+gbc = train.train_grad_boost(X_train, y_train)
+
+import models.predict_model as pred
+importlib.reload(models.predict_model)
+y_pred_gbc = pred.pred_grad_boost(X_test, gbc)
+
+pred.score_grad_boost(y_test, y_pred_gbc, gbc)
